c30ef459d2131c9e9749252eab55501103861ebc
rappi-academy <javier.merchan@rappi.com>
2026-03-26 22:32:15 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../bd-academy-firebase.jsx'
+import App from '../bd-academy-firebase.jsx' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
